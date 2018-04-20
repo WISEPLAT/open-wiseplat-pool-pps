bf0d39cf3019d8714a5461bb4299e39a5e323941
WISEPLAT <stroy-detal.com@yandex.ru>
2018-04-20 14:43:22 +0300

    open-wiseplat-pool-pps

---
+++ www/app/router.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('account', { path: '/account/:login' }, function() {
+    this.route('shifts');
+    this.route('today');
+    this.route('payouts');
+  });
+  this.route('not-found');
+
+  this.route('blocks', function() {
+    this.route('immature');
+    this.route('pending');
+  });
+
+  this.route('help');
+  this.route('payments');
+  this.route('miners');
+  this.route('about');
+});
+
+export default Router;
